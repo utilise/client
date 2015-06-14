f88c4ca80a576398c0683ab1788a5369c40efaaa
pemrouz <p3pedram@hotmail.com>
2015-06-14 21:10:01 +0100

    100% coverage

--- test.js
+++ test.js
@@ -3,7 +3,12 @@ var expect = require('chai').expect
 
 describe('client', function() {
 
-  it('should detect running in node', function(){
+  /* istanbul ignore next */
+  typeof window !== 'undefined' && it('should detect running in browser', function(){
+    expect(client).to.be.ok
+  })
+
+  typeof window === 'undefined' && it('should detect running in node', function(){
     expect(client).to.not.be.ok
   })
 
